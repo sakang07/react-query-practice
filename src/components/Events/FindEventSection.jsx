6c998bef01e1f76e51790322d6efc00dba81d213
Leslie <leslie@chequer.io>
2023-12-10 20:58:07 +0900

    feat: 동적 queryKey 넘기기, signal 객체로 Abort 처리하기

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,10 +1,51 @@
-import { useRef } from 'react';
+import {useRef, useState} from 'react';
+import {useQuery} from "@tanstack/react-query";
+import {fetchEvents} from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const {data, isPending, isError, error} = useQuery({
+    // queryKey에 동적인 값을 전달하여 같은 쿼리로 다른 데이터를 캐싱하거나 재사용할 수 있다.
+    queryKey: ['events', {search: searchTerm}],
+    queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }), // submit시의 검색어를 url로 전달
+  });
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if (isPending) {
+    content = <LoadingIndicator/>;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || 'Failed to fetch events.'}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event}/>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
+    // 리렌더를 일으키지 않는 ref의 값을 submit 이벤트 발생시 핸들러로 가져와 state를 업데이트.
+    setSearchTerm(searchElement.current.value);
   }
 
   return (
@@ -20,7 +61,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
--- src/util/http.js
+++ src/util/http.js
@@ -1,5 +1,17 @@
-export async function fetchEvents() {
-  const response = await fetch('http://localhost:3000/events');
+// fetch 함수에서 매개변수로 받는 값은 객체이다.
+// signal은 AbortController 객체의 signal 프로퍼티이다. 요청이 중단될 때 그 신호를 받는다.
+// 두 번째 인자는 호출부에서 넘긴 검색어를 받는다.
+export async function fetchEvents({ signal, searchTerm }) {
+  console.log(searchTerm)
+  let url = 'http://localhost:3000/events';
+
+  if (searchTerm) {
+    // 검색을 할 경우 searchTerm을 파라미터로 받아서 url에 추가한다.
+    url += '?search=' + searchTerm;
+  }
+  // signal을 객체 형태로 fetch의 두 번째 프로퍼티로 전달하면
+  // 사용자가 다른 페이지로 전환하는 등 요청을 취소하는 동작을 했을 때 HTTP 요청도 취소된다.
+  const response = await fetch(url, { signal: signal });
 
   if (!response.ok) {
     const error = new Error('An error occurred while fetching the events');
