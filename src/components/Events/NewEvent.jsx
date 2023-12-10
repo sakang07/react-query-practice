e08d032b925e8134ac6b0350c6a7a83109ff078e
Leslie <leslie@chequer.io>
2023-12-10 21:43:35 +0900

    feat: useMutation, mutate로 데이터 전송하기

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,50 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { useMutation } from "@tanstack/react-query";
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { createNewEvent } from '../../util/http.js';
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  // useMutation은 useQuery와는 달리 서버에 데이터를 보내는 과정에 관여한다.
+  // useMutation은 mutationFn을 통해 서버에 데이터를 보내는 함수를 전달받으며,
+  // useQuery처럼 컴포넌트가 실행되자마자 쿼리를 실행하지 않고 사용자가 원하는 타이밍에 실행한다.
+  // useQuery와 마찬가지로 data, isPending, isError, error 등의 상태를 반환한다.
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+  });
+  function handleSubmit(formData) {
+    // 원하는 타이밍에 mutate를 호출하여 서버에 데이터를 보낸다.
+    mutate({ event: formData });
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && 'Submitting...'}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+          )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Failed to create events"
+          message={
+            error.info?.message ||
+            'Failed to create event. Please try again later.'
+          }
+        />
+      )}
     </Modal>
   );
 }
